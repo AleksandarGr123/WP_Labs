068fe5372bc19ae146f414a9eceaa00c6d1fa35b
Aleksandar Grozdanoski <91129683+AleksandarGr123@users.noreply.github.com>
2025-11-02 20:18:45 +0100

    Add files via upload

---
+++ lab/src/main/java/mk/ukim/finki/wp/lab/service/impl/ChefServiceImpl.java
@@ -0,0 +1,50 @@
+package mk.ukim.finki.wp.lab.service.impl;
+
+import mk.ukim.finki.wp.lab.model.Chef;
+import mk.ukim.finki.wp.lab.model.Dish;
+import mk.ukim.finki.wp.lab.repository.ChefRepository;
+import mk.ukim.finki.wp.lab.repository.DishRepository;
+import mk.ukim.finki.wp.lab.service.ChefService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ChefServiceImpl implements ChefService {
+    private final ChefRepository chefRepository;
+    private final DishRepository dishRepository;
+
+    public ChefServiceImpl(ChefRepository ch, DishRepository d) {
+        this.chefRepository = ch;
+        this.dishRepository = d;
+    }
+
+    @Override
+    public List<Chef> listChefs() {
+        return this.chefRepository.findAll();
+    }
+
+    @Override
+    public Chef findById(Long id) {
+        return this.chefRepository.findById(id).orElse(null);
+    }
+
+
+
+    @Override
+    public Chef addDishToChef(Long chefId, String dishId) {
+        Chef chef = this.findById(chefId);
+        Dish dish = dishRepository.findByDishId(dishId);
+
+        if (chef == null || dish == null) {
+            return null;
+        }
+
+        chef.getDishes().add(dish);
+        chefRepository.save(chef);
+
+        return chef;
+    }
+
+
+}
